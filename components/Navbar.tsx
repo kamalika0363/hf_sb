fad7f5aad08a381cfee67f84466f38bf9ce81402
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-08-15 20:18:32 +0530

    footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="text-gray-300 rounded-xl sm:p-5 p-3 sm:ml-9 sm:mr-9 m-4 mt-6 border-2 border-zinc-900 bg-[#070707] font-semibold flex justify-between items-center">
+    <div className="text-gray-300 rounded-xl sm:p-5 p-3 sm:ml-9 sm:mr-9 m-4 mt-6 border-2 border-zinc-950 bg-[#070707] font-semibold flex justify-between items-center">
       <button className="h-6 w-6" >
         <img src="menu.svg" alt="hamburger" />
         </button>
